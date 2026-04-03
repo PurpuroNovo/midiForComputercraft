cc6659447d25f3e00ac9f279fb69c799c4055c55
PurpuroNovo <120147271+PurpuroNovo@users.noreply.github.com>
2026-04-02 23:11:08 -0300

    Added miditool for viewing midi devices and setting defaults

---
+++ miditool.lua
@@ -0,0 +1,57 @@
+if not midi then require("/midi") end
+
+function showUsage()
+    print("Usage: midi list")
+    print("Usage: midi setoutput <id>")
+    print("Usage: midi setinput <id>")
+end
+
+local persistent = settings.get("midi.persistent", false)
+if not persistent then
+    term.setTextColour(colors.yellow)
+    print("midi.persistent is false, this means the midi api isn't permanent, therefore devices, and default inputs/outputs from other programs don't stick")
+    print("if you want advanced use, \"set midi.persistent true\", if you just want to play MIDIs, keep as is")
+    term.setTextColour(colors.white)
+end
+
+function logDevice(device)
+    local id = device.id
+    local isDefaultInput = midi.defaultInputID == id
+    local isDefaultOutput = midi.defaultOutputID == id
+    local isDefault = isDefaultInput or isDefaultOutput
+    print(id .. " \"" .. device.name .. "\" " .. device.mode .. (isDefault and (isDefaultInput and " (Default input)" or " (Default output)") or ""))
+end
+
+if arg[1] == "list" then
+    print("Avaliable MIDI devices")
+    for id, device in pairs(midi.devices) do
+        logDevice(device)
+    end
+elseif arg[1] == "setoutput" or arg[1] == "setinput" then
+    if arg[2] then
+        local id = tonumber(arg[2])
+        assert(type(id) == "number", "Invalid ID")
+        local device = midi.devices[id]
+        assert(device, "Couldn't find device with id " .. id)
+        if arg[1] == "setoutput" then assert(device.isOutput, "Cant set an input device as the default output") end
+        if arg[1] == "setinput" then assert(device.isInput, "Can't set an output device as the default input") end
+        if arg[1] == "setoutput" then midi.defaultOutputID = id end
+        if arg[1] == "setinput" then midi.defaultInputID = id end
+    else
+        local device = nil
+        if arg[1] == "setinput" then
+            print("Showing default input device:")
+            device = midi.devices[midi.defaultInputID]
+        else
+            print("Showing default output device:")
+            device = midi.devices[midi.defaultOutputID]
+        end
+        if device then
+            logDevice(device)
+        else
+            print("(No device found)")
+        end
+    end
+else
+    showUsage()
+end
